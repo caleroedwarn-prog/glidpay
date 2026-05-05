f245923fa901a738f8b0cab293fcd325391966b1
caleroedwarn-prog <caleroedwarn@gmail.com>
2026-05-05 19:24:49 +0100

    I define what the context holds

--- glidpay-frontend/src/context/AuthContext.tsx
+++ glidpay-frontend/src/context/AuthContext.tsx
@@ -1,5 +1,15 @@
 import { createContext, useContext, useState} from "react"
+import { type AuthUser } from "../types/auth"
 
 
 // I define what the context holds
+
+type AuthContextType = {
+    user: AuthUser | null,
+    isLoggedIn: boolean,
+    isLoading: boolean,
+    login: (user: AuthUser,token: string) => void,
+    logout: () => void
+
+}
 
