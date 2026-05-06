351f0a604552d8e09945348f8a00429304be8762
caleroedwarn-prog <caleroedwarn@gmail.com>
2026-05-06 11:58:14 +0100

    created a handle change type

--- glidpay-frontend/src/pages/LoginPage.tsx
+++ glidpay-frontend/src/pages/LoginPage.tsx
@@ -2,11 +2,25 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../hooks/useAuth";
 import { loginUser } from "../api/auth";
-import { LoginRequest } from "../types/auth";
+import { type LoginRequest } from "../types/auth";
 function LoginPage(){
     
     const { login } = useAuth()
-    const navigate = useNavigate
+    const navigate = useNavigate()
+
+    const [form, setForm] = useState<LoginRequest>({
+        username: "",
+        password: ""
+    })
+
+    const[error, setError] = useState("")
+    const[isLoading, setIsLoading] = useState(false)
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setForm({...form, [e.target.name]: e.target.value})
+    }
+    
+        
 
 }
 export default Login
